c2b6ca6f7fb8d8357b0df875db3728f6e45e9330
J-J-B-J <79617327+J-J-B-J@users.noreply.github.com>
2024-11-27 15:39:51 +1100

    Update debugServer.sh
    Oopsie new info learned
    
    Co-Authored-By: Myles Eftos <myles@madpilot.com.au>

--- resources/debugServer.sh
+++ resources/debugServer.sh
@@ -2,9 +2,6 @@ echo "Enter your API key: "
 read -r API_KEY
 echo "Enter your Site ID: "
 read -r SITE_ID
-mkdir v1
-mkdir v1/sites
-mkdir v1/sites/$SITE_ID
-mkdir v1/sites/$SITE_ID/prices
+mkdir -p v1/sites/$SITE_ID/prices
 curl -H "Authorization: Bearer $API_KEY" https://api.amber.com.au/v1/sites/$SITE_ID/prices/current > v1/sites/$SITE_ID/prices/current
 python3 -m http.server
